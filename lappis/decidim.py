900e593e5884bc94bdebe14679b204d4932be1c4
Paulo Gonçalves Lima <paulogoncalves436@gmail.com>
2023-09-05 13:20:55 -0300

    refactor(decidimHook): trocado o nome de authenticate_decidim para decidim

---
+++ lappis/decidim.py
@@ -0,0 +1,41 @@
+"""Class to authenticate at decidim with airflow connection host, login
+and password.
+"""
+
+# pylint: disable=invalid-name
+
+import logging
+from urllib.parse import urljoin
+import requests
+from contextlib import closing
+from airflow.hooks.base import BaseHook
+import pandas as pd
+import re
+from inflection import underscore
+import inflect
+import numpy as np
+from datetime import datetime
+from bs4 import BeautifulSoup
+
+
+class DecidimHook(BaseHook):
+    def __init__(self, conn_id: str):
+        conn_values = self.get_connection(conn_id)
+        self.api_url = conn_values.host
+        self.auth_url = urljoin(self.api_url, "api/sign_in")
+        self.payload = {
+            "user[email]": conn_values.login,
+            "user[password]": conn_values.password,
+        }
+
+    def run_graphql_post_query(self, graphql_query):
+        response = self.get_session().post(self.api_url, json={"query": graphql_query})
+        status_code = response.status_code
+        assert status_code == 200, logging.ERROR(
+            f"""Query:
+                                                        {graphql_query}
+                                                     has returned status code: {status_code}
+                                                    """
+        )
+
+        return response.json()
