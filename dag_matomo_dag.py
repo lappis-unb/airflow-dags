70d7b3c1406d4e40d2c3b042d450c3ae0d46e22c
Nitai Bezerra da Silva <nitaibezerra@protonmail.com>
2023-10-04 17:41:54 -0300

    docs(matomo_extractor): add docs

--- dag_matomo_dag.py
+++ dag_matomo_dag.py
@@ -1,3 +1,12 @@
+"""
+Matomo Data Download DAG
+
+This Airflow DAG is responsible for fetching analytics data from a Matomo instance
+and saving it as CSV files to a MinIO bucket. The data fetched ranges from visitor
+summaries, action types, referrer sources, and user device types. This DAG is intended
+to run daily and fetch the data for the specific day of execution.
+"""
+
 from datetime import datetime, timedelta
 from airflow.decorators import task, dag
 from airflow.hooks.base_hook import BaseHook
@@ -63,8 +72,20 @@ def save_to_minio(data, module, method, execution_date):
         schedule_interval='@daily',
         start_date=datetime(2023, 5, 1),
         catchup=False,
+        doc_md=__doc__,
 )
 def matomo_data_download():
+    """
+    matomo_data_download DAG
+
+    This DAG orchestrates the fetching of various analytics data points from a Matomo
+    instance and saves them as CSV files to a MinIO bucket. It dynamically creates tasks
+    for each module-method combination specified in the `endpoints` list and fetches data
+    for the specific execution day.
+
+    Returns:
+        function: An instance of the matomo_data_download DAG
+    """
 
     # List of endpoints and methods to call
     endpoints = [
