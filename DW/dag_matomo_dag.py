42f3587c0e3d633aa38e4f84b0d4a36f35049b28
Nitai Bezerra da Silva <nitaibezerra@protonmail.com>
2023-11-09 11:56:34 +0000

    feat(matomo_ingestion): add temporal columns

--- DW/dag_matomo_dag.py
+++ DW/dag_matomo_dag.py
@@ -37,6 +37,36 @@ def _generate_s3_filename(module, method, execution_date):
     return f'{module}_{method}_{execution_date.strftime("%Y-%m-%d")}.csv'
 
 
+def add_temporal_columns(
+        df: pd.DataFrame,
+        execution_date: datetime) -> pd.DataFrame:
+    """
+    Adds temporal columns to the DataFrame based on the execution date.
+
+    Args:
+        df (pd.DataFrame): The original DataFrame without temporal columns.
+        execution_date (datetime): The execution date to base the temporal columns on.
+
+    Returns:
+        pd.DataFrame: The DataFrame with added temporal columns.
+    """
+    event_day_id = int(execution_date.strftime('%Y%m%d'))
+    available_day = execution_date + timedelta(days=1)
+    available_day_id = int(available_day.strftime('%Y%m%d'))
+    available_month_id = int(available_day.strftime('%Y%m'))
+    available_year_id = int(available_day.strftime('%Y'))
+    writing_day_id = int(datetime.now().strftime('%Y%m%d'))
+
+    # Add the temporal columns to the DataFrame
+    df['event_day_id'] = event_day_id
+    df['available_day_id'] = available_day_id
+    df['available_month_id'] = available_month_id
+    df['available_year_id'] = available_year_id
+    df['writing_day_id'] = writing_day_id
+
+    return df
+
+
 class MatomoDagGenerator:
 
     endpoints = [
@@ -144,11 +174,13 @@ class MatomoDagGenerator:
         # Read the CSV content into a pandas DataFrame
         df = pd.read_csv(StringIO(csv_content))
 
+        df_with_temporal = add_temporal_columns(df, execution_date)
+
         # Establish a connection to the PostgreSQL database
         pg_hook = PostgresHook(postgres_conn_id='dw_postgres')
 
         # Insert the data into the PostgreSQL table
-        df.to_sql(
+        df_with_temporal.to_sql(
             name=f'{module}_{method}',
             con=pg_hook.get_sqlalchemy_engine(),
             if_exists='append',
