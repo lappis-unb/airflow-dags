d2e1512b42592e742620047c79fc388a24c750e5
Paulo Gonçalves Lima <paulogoncalves436@gmail.com>
2024-03-01 18:58:40 -0300

    refactor(lint): corrige erros de lint

---
+++ dags/processes_confs/dag_matomo_segmentation.py
@@ -0,0 +1,49 @@
+import logging
+from datetime import datetime, timedelta
+from itertools import chain
+from pathlib import Path
+
+import requests
+from airflow.decorators import dag, task
+from airflow.hooks.base_hook import BaseHook
+from airflow.models import Variable
+
+from plugins.components.base_component.component import ComponentBaseHook
+from plugins.graphql.hooks.graphql_hook import GraphQLHook
+
+PARTICIPATORY_SPACES = [
+    "participatory_processes",
+    "initiatives",
+    "consultations",
+    "conferences",
+    "assemblies",
+]
+DECIDIM_CONN_ID = "bp_conn_prod"
+MATOMO_CONN_ID = "matomo_conn"
+SEGMENTED_IDS_VAR = "matomo_segmentation_ids"
+ACCEPTED_TYPES = ["Proposals", "Meetings", "Surveys"]
+
+
+def _get_participatory_space_mapped_to_query_file(participatory_spaces: list[str]):
+    queries_folder = Path(__file__).parent.joinpath("./queries/matomo_segmentation")
+    queries_files = {
+        participatory_space: queries_folder.joinpath(f"./components_in_{participatory_space}.gql")
+        for participatory_space in participatory_spaces
+    }
+    for query in queries_files.values():
+        assert query.exists()
+    return queries_files
+
+
+QUERIES = _get_participatory_space_mapped_to_query_file(PARTICIPATORY_SPACES)
+
+
+DEFAULT_ARGS = {
+    "owner": "Paulo/Isaque",
+    "depends_on_past": False,
+    "email_on_failure": True,
+    "email_on_retry": False,
+    "retries": 0,
+    "retry_delay": timedelta(minutes=5),
+}
+
