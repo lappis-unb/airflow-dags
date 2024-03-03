16ce5d298b29374fe6dc5ac4a04fddc5f102f368
Paulo Gonçalves Lima <paulogoncalves436@gmail.com>
2024-03-03 02:51:32 -0300

    feat(dag-participatory-texts): adiciona a geracao do grafico de participacao

--- plugins/reports/script.py
+++ plugins/reports/script.py
@@ -1,14 +1,45 @@
+import base64
+from contextlib import closing
 from datetime import datetime
 from io import BytesIO
 from pathlib import Path
 
+import plotly.express as px
+import plotly.io as pio
 from jinja2 import Environment, FileSystemLoader
 from weasyprint import HTML
 
 
-def generate_total_contributing_graph(
-    total_comments: int,
-): ...
+def encode_graph(graph):
+    with closing(BytesIO()) as buffer:
+        pio.write_image(graph, buffer, format="svg")
+        buffer.seek(0)
+        graph = base64.b64encode(buffer.getvalue()).decode("utf-8")
+
+    return graph
+
+
+def generate_participation_graph(
+    total_comments: int, total_unique_participants: int, width: int = 704, height: int = 480
+):
+    data = {
+        "Metrica": ["Contribuições", "Participantes"],
+        "Valores": [total_comments, total_unique_participants],
+    }
+
+    # Create a bar chart using Plotly Express
+    fig = px.bar(data, x="Valores", y="Metrica", text="Valores", labels={"Valores": "Total"})
+
+    # Customize the layout if needed
+    fig.update_layout(
+        yaxis_title="",
+        xaxis_title="",
+        bargap=0.3,
+        width=width,  # Set the width of the plot
+        height=height,  # Set the height of the plot
+    )
+    fig.update_traces(marker_color=["#1f77b4", "#ff7f0e"], insidetextanchor="middle")
+    return encode_graph(fig)
 
 
 def create_report_pdf(report_data):
