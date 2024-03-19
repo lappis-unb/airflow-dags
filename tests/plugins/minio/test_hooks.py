c0a2a07552f4388157db9041a3bf2ce65f093a4a
Paulo Gonçalves Lima <paulogoncalves436@gmail.com>
2024-03-18 22:07:49 -0300

    tests(minio-hook): adiciona casos de testes para a criacao do miniohook

--- tests/plugins/minio/test_hooks.py
+++ tests/plugins/minio/test_hooks.py
@@ -1,11 +1,13 @@
 import pytest
 from airflow.models.connection import Connection
+from airflow.exceptions import AirflowNotFoundException
 
+from plugins.minio.hooks import MinioHook
+from botocore.stub import Stubber
 
-@pytest.fixture
-def mock_get_component_type(mocker):
-    return mocker.patch("plugins.components.base_component.component.ComponentBaseHook.get_component_type")
+from boto3.session import Session
 
+# https://adamj.eu/tech/2019/04/22/testing-boto3-with-pytest-fixtures/
 
 @pytest.fixture
 def mock_connection(mocker):
@@ -18,3 +20,31 @@ def mock_connection(mocker):
 
     mock_connection_uri = mock_connection.get_uri()
     mocker.patch.dict("os.environ", AIRFLOW_CONN_MINIO_CONN=mock_connection_uri)
+
+@pytest.fixture
+def minio_stuber():
+    with Stubber(Session.client) as stubber:
+        yield stubber
+        stubber.assert_no_pending_responses()
+
+def test_success_create_minio_hook(mock_connection):
+    hook = MinioHook("minio_conn")
+    assert hook._host == "https://minio:9000/"
+    assert hook._access_key == "lappis"
+    assert hook._secret == "lappisrocks"
+    assert hook._session is None
+
+
+@pytest.mark.parametrize("conn_id", [(12), (None), ([])])
+def test_fail_create_minio_hook(conn_id):
+    with pytest.raises(AssertionError):
+        MinioHook(conn_id)
+
+
+def test_fail_create_minio_hook_conn_not_exists():
+    with pytest.raises(AirflowNotFoundException):
+        MinioHook("not_exists")
+
+
+def test_minio__get_session(mock_connection, minio_stuber: Stubber):
+    ...
