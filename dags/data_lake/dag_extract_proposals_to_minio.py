beba15daf25c852e542cff20c8ddf0439aa431ce
joao-amoedo <jp.advc@gmail.com>
2024-04-01 19:32:30 -0300

    fix empty data and merge with load dag

--- dags/data_lake/dag_extract_proposals_to_minio.py
+++ dags/data_lake/dag_extract_proposals_to_minio.py
@@ -3,12 +3,16 @@ import json
 from datetime import datetime, timedelta
 
 import pandas as pd
-from airflow.decorators import dag, task
+from airflow.decorators import dag, task, task_group
 from airflow.hooks.base_hook import BaseHook
 from airflow.operators.empty import EmptyOperator
 from minio import Minio
-
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+from airflow.providers.amazon.aws.operators.s3 import S3CreateBucketOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 from plugins.graphql.hooks.graphql_hook import GraphQLHook
+
+import logging
 
 # Vai ser trocado para salvar em arquivo
 QUERY = """
@@ -100,12 +104,16 @@ query teste ($start_date: String!, $end_date: String!) {
 
 """
 DECIDIM_CONN_ID = "api_decidim"
-MINIO_CONN_ID = "minio_connection_id"
+MINIO_CONN_ID = "minio_connection_id_test"
 MINIO_BUCKET = "brasil-participativo-daily-csv"
 COMPONENT_TYPE_TO_EXTRACT = "Proposals"
-
+TABLE_NAME = "proposals"
+SCHEMA = "raw"
+RETRIES = 0
 LANDING_ZONE_FILE_NAME = "landing_zone/proposals{date_file}.json"
+PROCESSING_FILE_NAME = "processing/proposals{date_file}.json"
 PROCESSED_FILE_NAME = "processed/proposals{date_file}.csv"
+POSTGRES_CONN_ID = "conn_postgres"
 
 
 def flatten_structure_with_additional_fields(data):
@@ -176,6 +184,36 @@ def flatten_structure_with_additional_fields(data):
     return flattened_data
 
 
+def add_temporal_columns(df: pd.DataFrame, execution_date: datetime) -> pd.DataFrame:
+    """
+    Adds temporal columns to the DataFrame based on the execution date.
+
+    Args:
+    ----
+        df (pd.DataFrame): The original DataFrame without temporal columns.
+        execution_date (datetime): The execution date to base the temporal columns on.
+
+    Returns:
+    -------
+        pd.DataFrame: The DataFrame with added temporal columns.
+    """
+    event_day_id = int(execution_date.strftime("%Y%m%d"))
+    available_day = execution_date + timedelta(days=1)
+    available_day_id = int(available_day.strftime("%Y%m%d"))
+    available_month_id = int(available_day.strftime("%Y%m"))
+    available_year_id = int(available_day.strftime("%Y"))
+    writing_day_id = int(datetime.now().strftime("%Y%m%d"))
+
+    # Add the temporal columns to the DataFrame
+    df["event_day_id"] = event_day_id
+    df["available_day_id"] = available_day_id
+    df["available_month_id"] = available_month_id
+    df["available_year_id"] = available_year_id
+    df["writing_day_id"] = writing_day_id
+
+    return df
+
+
 def dict_safe_get(_dict: dict, key: str):
     """
     Retorna o valor associado à chave especificada em um dicionário.
@@ -198,28 +236,25 @@ def dict_safe_get(_dict: dict, key: str):
     return value
 
 
-def _get_minio():
+def _check_and_create_schema(engine, schema):
     """
-    Retorna uma instância do cliente Minio.
+    Check if a schema exists in the database, if not, create it.
 
-    configurado com as informações de conexão fornecidas pelo hook Minio.
-
-    Returns:
-    -------
-      Minio: Uma instância do cliente Minio
-      configurado com as informações de conexão fornecidas pelo hook Minio.
+    Args:
+    ----
+    engine (sqlalchemy.engine.Engine): The SQLAlchemy engine instance.
+    schema (str): The schema name.
     """
-    minio_conn = BaseHook.get_connection(MINIO_CONN_ID)
-    minio_host = minio_conn.host.split("//")[1]
-    minio_access_key = minio_conn.login
-    minio_secret_access = minio_conn.password
-    client = Minio(
-        minio_host,
-        access_key=minio_access_key,
-        secret_key=minio_secret_access,
-        secure=False,
-    )
-    return client
+    with engine.connect() as connection:
+        result = connection.execute(
+            f"""SELECT EXISTS(SELECT 1 FROM
+             information_schema.schemata
+             WHERE schema_name = '{schema}');"""
+        )
+        exists = result.scalar()
+        if not exists:
+            connection.execute(f"CREATE SCHEMA {schema};")
+            logging.info("Schema %s created successfully.", schema)
 
 
 @dag(
@@ -239,8 +274,28 @@ def _get_minio():
 def fetch_process_and_clean_proposals():
     """DAG que extrai dados de propostas de um GraphQL API e os armazena em um bucket MinIO."""
 
-    @task(provide_context=True)
-    def get_data(**context):
+    @task_group(group_id="minio_tasks")
+    def minio_tasks():
+        @task.branch(retries=0)
+        def verify_bucket():
+            """
+            Verifies if the specified bucket exists in
+            the MinIO server.
+
+            Returns:
+              str: The name of the task to create the bucket if it doesn't exist.
+            """
+            if not (S3Hook(aws_conn_id=MINIO_CONN_ID)
+                    .check_for_bucket(bucket_name=MINIO_BUCKET)):
+                return "minio_tasks.create_bucket"
+
+        create_bucket = S3CreateBucketOperator(
+            task_id="create_bucket", bucket_name=MINIO_BUCKET, aws_conn_id=MINIO_CONN_ID
+        )
+        verify_bucket() >> create_bucket
+
+    @task(provide_context=True, retries=0, trigger_rule="none_failed")
+    def extract_data(**context):
         """
         Fetches data from a GraphQL API and stores it in a MinIO bucket.
 
@@ -262,42 +317,161 @@ def fetch_process_and_clean_proposals():
             hook.api_url,
             json={"query": QUERY, "variables": {"start_date": f"{date}", "end_date": f"{next_date}"}},
         )
-        dado = response.json()
+        # dado = response.json()
+        dado = response.text
         # Store data in MinIO bucket
-        minio = _get_minio()
-        minio.put_object(
-            MINIO_BUCKET,
-            LANDING_ZONE_FILE_NAME.format(date_file=date_file),
-            io.BytesIO(json.dumps(dado).encode()),
-            len(json.dumps(dado).encode()),
+        S3Hook(aws_conn_id=MINIO_CONN_ID).load_string(
+            string_data=dado,
+            bucket_name=MINIO_BUCKET,
+            key=LANDING_ZONE_FILE_NAME.format(date_file=date_file),
+            replace=True,
         )
 
-    @task(provide_context=True, retries=3, retry_delay=timedelta(seconds=5))
-    def salva_minio(**context):
-        date_file = context["execution_date"].strftime("%Y%m%d")
-        minio = _get_minio()
-        dado = minio.get_object(MINIO_BUCKET, LANDING_ZONE_FILE_NAME.format(date_file=date_file))
-        dado = json.loads(dado.read())
-        dado = flatten_structure_with_additional_fields(dado)
-        df = pd.DataFrame(dado)
-        csv_buffer = io.StringIO()
-        df.to_csv(csv_buffer, index=False)
-        minio.put_object(
-            MINIO_BUCKET,
-            PROCESSED_FILE_NAME.format(date_file=date_file),
-            io.BytesIO(csv_buffer.getvalue().encode()),
-            len(csv_buffer.getvalue().encode()),
-        )
+    @task_group
+    def transform():
+        @task(provide_context=True, retries=0, retry_delay=timedelta(seconds=5))
+        def transform_data(**context):
+            """
+            Transforms the data by flattening the structure and saving it as a CSV file in MinIO.
 
-    @task(provide_context=True)
-    def delete_landing_zone_file(**context):
-        date_file = context["execution_date"].strftime("%Y%m%d")
-        minio = _get_minio()
-        minio.remove_object(MINIO_BUCKET, LANDING_ZONE_FILE_NAME.format(date_file=date_file))
+            Args:
+              **context: The context dictionary containing the execution date.
+
+            Returns:
+              None
+            """
+            date_file = context["execution_date"].strftime("%Y%m%d")
+            minio = S3Hook(aws_conn_id=MINIO_CONN_ID)
+            dado = minio.read_key(
+                key=LANDING_ZONE_FILE_NAME.format(date_file=date_file), bucket_name=MINIO_BUCKET
+            )
+            dado = json.loads(dado)
+            dado = flatten_structure_with_additional_fields(dado)
+            df = pd.DataFrame(dado)
+            csv_buffer = io.StringIO()
+            df.to_csv(csv_buffer, index=False)
+            minio.load_string(
+                string_data=csv_buffer.getvalue(),
+                bucket_name=MINIO_BUCKET,
+                key=PROCESSING_FILE_NAME.format(date_file=date_file),
+                replace=True,
+            )
+
+        @task(provide_context=True, retries=0)
+        def delete_landing_zone_file(**context):
+            """
+            Deletes a file from the landing zone in MinIO.
+
+            Args:
+              context (dict): The context dictionary containing the execution date.
+
+            Returns:
+              None
+            """
+            date_file = context["execution_date"].strftime("%Y%m%d")
+            minio = S3Hook(aws_conn_id=MINIO_CONN_ID)
+            print(dir(minio))
+            minio.delete_objects(bucket=MINIO_BUCKET, keys=LANDING_ZONE_FILE_NAME.format(date_file=date_file))
+
+        transform_data() >> delete_landing_zone_file()
+
+    @task_group(group_id="load")
+    def load():
+        empty = EmptyOperator(task_id="empty")
+
+        @task.branch(provide_context=True)
+        def check_empty_file(**context):
+            """
+            Checks if the file in Minio bucket is empty.
+
+            Args:
+              context (dict): The context dictionary containing execution information.
+
+            Returns:
+              str: The branch to follow based on whether the file is empty or not.
+            """
+            minio = S3Hook(aws_conn_id=MINIO_CONN_ID)
+            date_file = context["execution_date"].strftime("%Y%m%d")
+            dado = minio.read_key(
+                key=PROCESSING_FILE_NAME.format(date_file=date_file), bucket_name=MINIO_BUCKET
+            )
+            if len(dado.strip()) == 0:
+                logging.warning("No data found for %s.", date_file)
+                return "load.empty"
+            return "load.check_and_create_schema"
+
+        @task(retries=RETRIES, retry_delay=timedelta(minutes=3))
+        def check_and_create_schema():
+            """
+            Checks if the 'proposals' schema exists in the PostgreSQL database and creates it if it doesn't exist.
+
+            Returns:
+              None
+            """
+            engine = PostgresHook(postgres_conn_id=POSTGRES_CONN_ID).get_sqlalchemy_engine()
+            _check_and_create_schema(engine, "proposals")
+
+        @task(retries=RETRIES, retry_delay=timedelta(minutes=3))
+        def check_and_create_table():
+            """
+            Checks if the 'proposals' table exists in the database schema and creates it if it doesn't exist.
+
+            Returns:
+              None
+            """
+            engine = PostgresHook(postgres_conn_id=POSTGRES_CONN_ID).get_sqlalchemy_engine()
+            _check_and_create_schema(engine, "proposals")
+
+        @task(provide_context=True, retries=RETRIES, retry_delay=timedelta(minutes=3))
+        def save_data_potgres(**context):
+            """
+            Task to save data from Minio to PostgreSQL.
+
+            Args:
+              context (dict): The context dictionary containing execution information.
+
+            Returns:
+              None
+            """
+            date_file = context["execution_date"].strftime("%Y%m%d")
+            minio = S3Hook(aws_conn_id=MINIO_CONN_ID)
+            data = minio.read_key(
+                key=PROCESSING_FILE_NAME.format(date_file=date_file), bucket_name=MINIO_BUCKET
+            )
+            csv_file = io.StringIO(data)
+            df = pd.read_csv(csv_file)
+            df = add_temporal_columns(df, context["execution_date"])
+            ## Configure the postgres hook and insert the data
+            engine = PostgresHook(postgres_conn_id=POSTGRES_CONN_ID).get_sqlalchemy_engine()
+            df.to_sql(TABLE_NAME, con=engine, if_exists="append", index=False, schema=SCHEMA)
+
+        @task(retries=RETRIES)
+        def check_create_primary_key():
+            """
+            This function checks if a primary key exists for the table.
+            If not, it creates a new primary key.
+            """
+            primary_keys = PostgresHook(postgres_conn_id=POSTGRES_CONN_ID).get_table_primary_key(
+                table=TABLE_NAME, schema=SCHEMA
+            )
+            if primary_keys:
+                return
+
+            engine = PostgresHook(postgres_conn_id=POSTGRES_CONN_ID).get_sqlalchemy_engine()
+            with engine.connect() as connection:
+                connection.execute(f"ALTER TABLE {SCHEMA}.{TABLE_NAME} ADD PRIMARY KEY (proposal_id);")
+
+        
+
+        _check_schema = check_and_create_schema()
+        check_empty_file() >> [empty, _check_schema]
+        _check_schema >> save_data_potgres() >> check_create_primary_key()
 
     start = EmptyOperator(task_id="start")
     end = EmptyOperator(task_id="end")
-    start >> get_data() >> salva_minio() >> delete_landing_zone_file() >> end
+
+    _extract_data = extract_data()
+    start >> minio_tasks() >> _extract_data >> transform() >> load() >> end
 
 
 fetch_process_and_clean_proposals()
