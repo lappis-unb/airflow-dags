96e9c24a5f5d07dc710f4744971a51ef6d75677c
Joyce <joycedionizio.jdm@gmail.com>
2024-04-02 10:52:06 -0300

    feat (reports): adiciona tratamento de dados vazios no render

--- plugins/reports/participatory_texts_report.py
+++ plugins/reports/participatory_texts_report.py
@@ -34,84 +34,104 @@ class ParticipatoryTextsReport(Report):
         matomo_devices_detection_csv: str,
     ):
 
-        proposals_ids = [proposal["id"] for proposal in report_data["proposals"]]
-        proposals_titles = [proposal["title"] for proposal in report_data["proposals"]]
-        votes_per_proposal = [proposal["vote_count"] for proposal in report_data["proposals"]]
-        total_comments_per_proposal = [proposal["total_comments"] for proposal in report_data["proposals"]]
+        if not report_data:
+            return self.template.render(
+                data={
+                    "document": {
+                        "title": f"Relatório {self.report_name}",
+                        "date": f"{self.start_date} até {self.end_date}",
+                    },
+                    "participation_graph": None,
+                    "participatory_texts": None,
+                    "top_devices_graph": None,
+                    "data_access": None,
+                    "device_graph": None,
+                    "map_graph": None,
+                    "comments": None,
+                }
+            )
+        else:
+            proposals_ids = [proposal["id"] for proposal in report_data["proposals"]]
+            proposals_titles = [proposal["title"] for proposal in report_data["proposals"]]
+            votes_per_proposal = [proposal["vote_count"] for proposal in report_data["proposals"]]
+            total_comments_per_proposal = [
+                proposal["total_comments"] for proposal in report_data["proposals"]
+            ]
 
-        top_devices_graph = self.bp_graphs.generate_top_devices(
-            titles=proposals_titles, total_comments=total_comments_per_proposal
-        )
-        participatory_texts_file = self.bp_tables.generate_participatory_texts_proposals(
-            proposals_ids,
-            proposals_titles,
-            votes_per_proposal,
-            total_comments_per_proposal,
-        )
+            top_devices_graph = self.bp_graphs.generate_top_devices(
+                titles=proposals_titles, total_comments=total_comments_per_proposal
+            )
+            participatory_texts_file = self.bp_tables.generate_participatory_texts_proposals(
+                proposals_ids,
+                proposals_titles,
+                votes_per_proposal,
+                total_comments_per_proposal,
+            )
 
-        participatory_texts_ids = [text["ID"] for text in participatory_texts_file]
-        participatory_texts_title = [text["Dispositivo"] for text in participatory_texts_file]
-        participatory_texts_comments = [text["Nº de comentários"] for text in participatory_texts_file]
-        participatory_texts_votes = [text["Nº de votos"] for text in participatory_texts_file]
+            participatory_texts_ids = [text["ID"] for text in participatory_texts_file]
+            participatory_texts_title = [text["Dispositivo"] for text in participatory_texts_file]
+            participatory_texts_comments = [text["Nº de comentários"] for text in participatory_texts_file]
+            participatory_texts_votes = [text["Nº de votos"] for text in participatory_texts_file]
 
-        participatory_texts = report_data["proposals"]
+            participatory_texts = report_data["proposals"]
 
-        state_rename = {
-            "accepted": "Aceita",
-            "withdrawn": "Retirada",
-            "rejected": "Rejeitada",
-        }
-
-        rename_state = lambda comments: [
-            {**comment, "status": state_rename.get(comment["status"], "Avaliando")} for comment in comments
-        ]
-
-        comments_data = [
-            {"title": text["title"], "comments": rename_state(text["comments"])}
-            for text in participatory_texts
-        ]
-
-        return self.template.render(
-            data={
-                "document": {
-                    "title": f"Relatório {self.report_name}",
-                    "date": f"{self.start_date} até {self.end_date}",
-                },
-                "participation_graph": {
-                    "label": "Gráfico De Participação",
-                    "file": self.bp_graphs.generate_participation_graph(
-                        report_data["total_comments"],
-                        report_data["total_unique_participants"],
-                    ),
-                },
-                "participatory_texts": {
-                    "ID": participatory_texts_ids,
-                    "Dispositivo": participatory_texts_title,
-                    "Nº de comentários": participatory_texts_comments,
-                    "Nº de votos": participatory_texts_votes,
-                },
-                "top_devices_graph": {
-                    "label": "Dispositivos mais utilizados",
-                    "file": top_devices_graph,
-                },
-                "data_access": MatotmoTables.generate_table_access_data_overview(
-                    matomo_visits_summary_csv, matomo_visits_frequency_csv
-                ),
-                "device_graph": {
-                    "label": "Detecção de Dispositivos",
-                    "file": self.matomo_graphs.try_build_graph(
-                        self.matomo_graphs.generate_device_graph,
-                        matomo_devices_detection_csv,
-                    ),
-                },
-                "map_graph": {
-                    "label": "Acesso por Estado",
-                    "file": self.matomo_graphs.try_build_graph(
-                        self.matomo_graphs.generate_brasil_access_map,
-                        matomo_user_country_csv,
-                        matomo_user_region_csv,
-                    ),
-                },
-                "comments": {"content": comments_data},
+            state_rename = {
+                "accepted": "Aceita",
+                "withdrawn": "Retirada",
+                "rejected": "Rejeitada",
             }
-        )
+
+            rename_state = lambda comments: [
+                {**comment, "status": state_rename.get(comment["status"], "Avaliando")}
+                for comment in comments
+            ]
+
+            comments_data = [
+                {"title": text["title"], "comments": rename_state(text["comments"])}
+                for text in participatory_texts
+            ]
+
+            return self.template.render(
+                data={
+                    "document": {
+                        "title": f"Relatório {self.report_name}",
+                        "date": f"{self.start_date} até {self.end_date}",
+                    },
+                    "participation_graph": {
+                        "label": "Gráfico De Participação",
+                        "file": self.bp_graphs.generate_participation_graph(
+                            report_data["total_comments"],
+                            report_data["total_unique_participants"],
+                        ),
+                    },
+                    "participatory_texts": {
+                        "ID": participatory_texts_ids,
+                        "Dispositivo": participatory_texts_title,
+                        "Nº de comentários": participatory_texts_comments,
+                        "Nº de votos": participatory_texts_votes,
+                    },
+                    "top_devices_graph": {
+                        "label": "Dispositivos mais utilizados",
+                        "file": top_devices_graph,
+                    },
+                    "data_access": MatotmoTables.generate_table_access_data_overview(
+                        matomo_visits_summary_csv, matomo_visits_frequency_csv
+                    ),
+                    "device_graph": {
+                        "label": "Detecção de Dispositivos",
+                        "file": self.matomo_graphs.try_build_graph(
+                            self.matomo_graphs.generate_device_graph,
+                            matomo_devices_detection_csv,
+                        ),
+                    },
+                    "map_graph": {
+                        "label": "Acesso por Estado",
+                        "file": self.matomo_graphs.try_build_graph(
+                            self.matomo_graphs.generate_brasil_access_map,
+                            matomo_user_country_csv,
+                            matomo_user_region_csv,
+                        ),
+                    },
+                    "comments": {"content": comments_data},
+                }
+            )
--- plugins/reports/proposals_report.py
+++ plugins/reports/proposals_report.py
@@ -17,72 +17,93 @@ class ProposalsReport(Report):
         matomo_user_country_csv: str,
         matomo_devices_detection_csv: str,
     ):
-        general_data = BrasilParticipativoTables.generate_table_proposals_overview(
-            votes_per_proposal=bp_df["proposal_total_votes"],
-            total_comments_per_proposal=bp_df["proposal_total_comments"],
-            proposal_states=bp_df["proposal_state"],
-        )
 
-        return self.template.render(
-            data={
-                "document": {
-                    "component": self.report_name,
-                    "title": f"Relatório {self.report_name}",
-                    "date": f"{self.start_date} até {self.end_date}",
-                },
-                "introduction": {
-                    "num_proposals": general_data["Propostas"],
-                    "total_votes": general_data["Votos"],
-                    "total_comments": general_data["Comentários"],
-                },
-                "general_data": BrasilParticipativoTables.generate_table_proposals_overview(
-                    votes_per_proposal=bp_df["proposal_total_votes"],
-                    total_comments_per_proposal=bp_df["proposal_total_comments"],
-                    proposal_states=bp_df["proposal_state"],
-                ),
-                "daily_graph": {
-                    "label": "Gráfico Diário",
-                    "file": self.bp_graphs.generate_daily_plot(
-                        proposals_publication_date=bp_df["proposal_published_at"],
+        if bp_df.empty:
+            return self.template.render(
+                data={
+                    "document": {
+                        "component": self.report_name,
+                        "title": self.report_name,
+                        "date": f"{self.start_date} até {self.end_date}",
+                    },
+                    "introduction": None,
+                    "general_data": None,
+                    "daily_graph": None,
+                    "state_distribution_graph": None,
+                    "data_access": None,
+                    "device_graph": None,
+                    "rank_temas": None,
+                    "top_proposals_filtered": None,
+                    "map_graph": None,
+                }
+            )
+        else:
+            general_data = BrasilParticipativoTables.generate_table_proposals_overview(
+                votes_per_proposal=bp_df["proposal_total_votes"],
+                total_comments_per_proposal=bp_df["proposal_total_comments"],
+                proposal_states=bp_df["proposal_state"],
+            )
+
+            return self.template.render(
+                data={
+                    "document": {
+                        "component": self.report_name,
+                        "title": f"Relatório {self.report_name}",
+                        "date": f"{self.start_date} até {self.end_date}",
+                    },
+                    "introduction": {
+                        "num_proposals": general_data["Propostas"],
+                        "total_votes": general_data["Votos"],
+                        "total_comments": general_data["Comentários"],
+                    },
+                    "general_data": BrasilParticipativoTables.generate_table_proposals_overview(
+                        votes_per_proposal=bp_df["proposal_total_votes"],
+                        total_comments_per_proposal=bp_df["proposal_total_comments"],
+                        proposal_states=bp_df["proposal_state"],
+                    ),
+                    "daily_graph": {
+                        "label": "Gráfico Diário",
+                        "file": self.bp_graphs.generate_daily_plot(
+                            proposals_publication_date=bp_df["proposal_published_at"],
+                            proposals_ids=bp_df["proposal_id"],
+                            total_comments_per_proposal=bp_df["proposal_total_comments"],
+                            votes_per_proposal=bp_df["proposal_total_votes"],
+                        ),
+                    },
+                    "state_distribution_graph": {
+                        "label": "Distribuição de Estados das Propostas",
+                        "file": self.bp_graphs.generate_state_distribution_donut(bp_df),
+                    },
+                    "data_access": MatotmoTables.generate_table_access_data_overview(
+                        matomo_visits_summary_csv, matomo_visits_frequency_csv
+                    ),
+                    "device_graph": {
+                        "label": "Detecção de Dispositivos",
+                        "file": self.matomo_graphs.try_build_graph(
+                            self.matomo_graphs.generate_device_graph,
+                            matomo_devices_detection_csv,
+                        ),
+                    },
+                    "rank_temas": BrasilParticipativoTables.generate_table_theme_ranking(
+                        proposals_categories=bp_df["proposal_category_title"],
                         proposals_ids=bp_df["proposal_id"],
                         total_comments_per_proposal=bp_df["proposal_total_comments"],
                         votes_per_proposal=bp_df["proposal_total_votes"],
                     ),
-                },
-                "state_distribution_graph": {
-                    "label": "Distribuição de Estados das Propostas",
-                    "file": self.bp_graphs.generate_state_distribution_donut(bp_df),
-                },
-                "data_access": MatotmoTables.generate_table_access_data_overview(
-                    matomo_visits_summary_csv, matomo_visits_frequency_csv
-                ),
-                "device_graph": {
-                    "label": "Detecção de Dispositivos",
-                    "file": self.matomo_graphs.try_build_graph(
-                        self.matomo_graphs.generate_device_graph,
-                        matomo_devices_detection_csv,
+                    "top_proposals_filtered": BrasilParticipativoTables.generate_top_proposals(
+                        proposals_ids=bp_df["proposal_id"],
+                        proposals_titles=bp_df["proposal_title"],
+                        proposals_category_titles=bp_df["proposal_category_title"],
+                        votes_per_proposal=bp_df["proposal_total_votes"],
+                        total_comments_per_proposal=bp_df["proposal_total_comments"],
                     ),
-                },
-                "rank_temas": BrasilParticipativoTables.generate_table_theme_ranking(
-                    proposals_categories=bp_df["proposal_category_title"],
-                    proposals_ids=bp_df["proposal_id"],
-                    total_comments_per_proposal=bp_df["proposal_total_comments"],
-                    votes_per_proposal=bp_df["proposal_total_votes"],
-                ),
-                "top_proposals_filtered": BrasilParticipativoTables.generate_top_proposals(
-                    proposals_ids=bp_df["proposal_id"],
-                    proposals_titles=bp_df["proposal_title"],
-                    proposals_category_titles=bp_df["proposal_category_title"],
-                    votes_per_proposal=bp_df["proposal_total_votes"],
-                    total_comments_per_proposal=bp_df["proposal_total_comments"],
-                ),
-                "map_graph": {
-                    "label": "Acesso por Estado",
-                    "file": self.matomo_graphs.try_build_graph(
-                        self.matomo_graphs.generate_brasil_access_map,
-                        matomo_user_country_csv,
-                        matomo_user_region_csv,
-                    ),
-                },
-            }
-        )
+                    "map_graph": {
+                        "label": "Acesso por Estado",
+                        "file": self.matomo_graphs.try_build_graph(
+                            self.matomo_graphs.generate_brasil_access_map,
+                            matomo_user_country_csv,
+                            matomo_user_region_csv,
+                        ),
+                    },
+                }
+            )
