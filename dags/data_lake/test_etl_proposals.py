4547cdc49ab4099b079df47a2cf98c60f50a5029
joao-amoedo <jp.advc@gmail.com>
2024-04-02 21:47:34 -0300

    add other tests for tasks

--- dags/data_lake/test_etl_proposals.py
+++ dags/data_lake/test_etl_proposals.py
@@ -1,31 +1,36 @@
+import os
+import io
+import sys
 import pandas as pd
 from datetime import datetime
-import os
-import sys
+from unittest import mock
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+
 from etl_proposals import (
     add_temporal_columns,
     dict_safe_get,
     _convert_dtype,
     _verify_bucket,
     _task_extract_data,
-    _delete_landing_zone_file
-    MINIO_CONN_ID,
-)
-from airflow.providers.amazon.aws.hooks.s3 import S3Hook
-from unittest import mock
-import json
-
-from etl_proposals import (
-    _get_df_transform_data,
+    _delete_landing_zone_file,
+    _check_empty_file,
+    _task_get_ids_from_table,
     _save_minio_processing,
-    PROCESSING_FILE_NAME,
+    _check_and_create_table,
+    _get_df_save_data_postgres,
+    _transform_data_save_data_postgres,
+    _task_move_file_s3,
+    MINIO_CONN_ID,
     MINIO_BUCKET,
     LANDING_ZONE_FILE_NAME,
+    PROCESSING_FILE_NAME,
+    TABLE_NAME,
+    SCHEMA,
+    PRIMARY_KEY,
+    PROCESSED_FILE_NAME,
 )
-
-import io
 
 
 def test_add_temporal_columns():
@@ -260,4 +265,244 @@ def test__delete_landing_zone_file():
     mock_s3hook.delete_objects.assert_called_once_with(
         bucket=MINIO_BUCKET,
         keys=LANDING_ZONE_FILE_NAME.format(date_file=date_file),
+    )
+
+
+def test__check_empty_file():
+    # Mock the context
+    context = {
+        "execution_date": datetime(2022, 1, 1),
+    }
+
+    # Mock the S3Hook
+    mock_s3hook = mock.MagicMock()
+    mock_s3hook.read_key.return_value = ""
+
+    # Patch the S3Hook
+    with mock.patch("etl_proposals.S3Hook", return_value=mock_s3hook):
+        # Call the function
+        result = _check_empty_file(**context)
+
+    # Assert the expected function calls and return value
+    date_file = context["execution_date"].strftime("%Y%m%d")
+    mock_s3hook.read_key.assert_called_once_with(
+        key=PROCESSING_FILE_NAME.format(date_file=date_file),
+        bucket_name=MINIO_BUCKET,
+    )
+    assert result == "load.empty_file"
+
+
+def test__check_empty_file_non_empty():
+    # Mock the context
+    context = {
+        "execution_date": datetime(2022, 1, 1),
+    }
+
+    # Mock the S3Hook
+    mock_s3hook = mock.MagicMock()
+    mock_s3hook.read_key.return_value = "some data"
+
+    # Patch the S3Hook
+    with mock.patch("etl_proposals.S3Hook", return_value=mock_s3hook):
+        # Call the function
+        result = _check_empty_file(**context)
+
+    # Assert the expected function calls and return value
+    date_file = context["execution_date"].strftime("%Y%m%d")
+    mock_s3hook.read_key.assert_called_once_with(
+        key=PROCESSING_FILE_NAME.format(date_file=date_file),
+        bucket_name=MINIO_BUCKET,
+    )
+    assert result == "load.check_and_create_table"
+
+
+def test__check_and_create_table():
+    # Mock the engine
+    mock_engine = mock.MagicMock()
+
+    # Mock the has_table method to return False
+    mock_engine.has_table.return_value = False
+
+    # Call the function
+    _check_and_create_table(mock_engine)
+
+    # Assert the expected function calls
+    mock_engine.has_table.assert_called_once_with(table_name=TABLE_NAME, schema=SCHEMA)
+    mock_engine.execute.assert_has_calls(
+        [
+            mock.call(
+                f"""
+    CREATE TABLE {SCHEMA}.{TABLE_NAME} (
+      main_title text NULL,
+      component_id int8 NULL,
+      component_name text NULL,
+      proposal_id int8 NOT NULL,
+      proposal_createdat timestamp NULL,
+      proposal_publishedat timestamp NULL,
+      proposal_updatedat timestamp NULL,
+      author_name text NULL,
+      author_nickname text NULL,
+      author_organization text NULL,
+      proposal_body text NULL,
+      category_name text NULL,
+      proposal_title text NULL,
+      authorscount int8 NULL,
+      userallowedtocomment bool NULL,
+      endorsementscount int8 NULL,
+      totalcommentscount int8 NULL,
+      versionscount int8 NULL,
+      votecount int8 NULL,
+      commentshavealignment bool NULL,
+      commentshavevotes bool NULL,
+      createdinmeeting bool NULL,
+      hascomments bool NULL,
+      official bool NULL,
+      fingerprint text NULL,
+      "position" int8 NULL,
+      reference text NULL,
+      "scope" text NULL,
+      state text NULL,
+      event_day_id int8 NULL,
+      available_day_id int8 NULL,
+      available_month_id int8 NULL,
+      available_year_id int8 NULL,
+      writing_day_id int8 NULL
+    );
+    """
+            ),
+            mock.call(
+                f"ALTER TABLE {SCHEMA}.{TABLE_NAME} ADD PRIMARY KEY ({PRIMARY_KEY});"
+            ),
+        ]
+    )
+
+    # Reset the mock calls
+    mock_engine.reset_mock()
+
+    # Mock the has_table method to return True
+    mock_engine.has_table.return_value = True
+
+    # Call the function again
+    _check_and_create_table(mock_engine)
+
+    # Assert the expected function calls
+    mock_engine.has_table.assert_called_once_with(table_name=TABLE_NAME, schema=SCHEMA)
+    mock_engine.execute.assert_not_called()
+
+
+def test__task_get_ids_from_empty_table():
+    # Mock the engine and connection
+    mock_connection = mock.MagicMock()
+    mock_engine = mock.MagicMock()
+    mock_engine.connect.return_value = mock_connection
+
+    # Mock the result of the SQL query
+    mock_result = mock.MagicMock()
+    mock_result.__iter__.return_value = []
+    mock_connection.execute.return_value = mock_result
+
+    # Call the function
+    result = _task_get_ids_from_table(mock_engine)
+
+    # Assert the expected function calls
+    mock_engine.execute.assert_not_called()
+    assert result == []
+
+
+def test__get_df_save_data_postgres():
+    # Mock the context
+    context = {
+        "execution_date": datetime(2022, 1, 1),
+    }
+
+    # Mock the S3Hook
+    mock_s3hook = mock.MagicMock()
+    mock_s3hook.read_key.return_value = "col1,col2\n1,a\n2,b\n3,c\n"
+
+    # Patch the S3Hook
+    with mock.patch("etl_proposals.S3Hook", return_value=mock_s3hook):
+        # Call the function
+        result = _get_df_save_data_postgres(context)
+
+    # Assert the expected DataFrame
+    expected_df = pd.DataFrame({"col1": [1, 2, 3], "col2": ["a", "b", "c"]})
+    pd.testing.assert_frame_equal(result, expected_df)
+
+
+def test__transform_data_save_data_postgres():
+    # Mock the context
+    context = {
+        "execution_date": datetime(2022, 1, 1),
+    }
+
+    # Create a sample DataFrame
+    df = pd.DataFrame(
+        {
+            "proposal_id": [10001, 10002, 10003],
+            "col1": [1, 2, 3],
+            "col2": ["a", "b", "c"],
+        }
+    )
+
+    # Define the proposal_ids to exclude
+    proposal_ids = [10001, 10003]
+
+    # Call the function
+    result = _transform_data_save_data_postgres(proposal_ids, context, df)
+    # Assert the expected DataFrame
+    expected_result = pd.DataFrame(
+        {
+            "proposal_id": [10002],
+            "col1": [2],
+            "col2": ["b"],
+            "event_day_id": [20220101],
+            "available_day_id": [20220102],
+            "available_month_id": [202201],
+            "available_year_id": [2022],
+            "writing_day_id": [int(datetime.now().strftime("%Y%m%d"))],
+        }
+    )
+
+    result.index = range(len(result))
+
+    pd.testing.assert_frame_equal(result, expected_result)
+
+
+def test__task_move_file_s3():
+    """
+    Test function for _task_move_file_s3.
+
+    This function tests the behavior of the _task_move_file_s3 function by mocking the context,
+    the S3Hook, and patching the S3Hook. It then calls the function and asserts the expected
+    function calls made to the mock S3Hook.
+
+    Returns:
+        None
+    """
+    # Mock the context
+    context = {
+        "execution_date": datetime(2022, 1, 1),
+    }
+
+    # Mock the S3Hook
+    mock_s3hook = mock.MagicMock()
+
+    # Patch the S3Hook
+    with mock.patch("etl_proposals.S3Hook", return_value=mock_s3hook):
+        # Call the function
+        _task_move_file_s3(context)
+
+    # Assert the expected function calls
+    date_file = context["execution_date"].strftime("%Y%m%d")
+    source_filename = PROCESSING_FILE_NAME.format(date_file=date_file)
+    dest_filename = PROCESSED_FILE_NAME.format(date_file=date_file)
+    mock_s3hook.copy_object.assert_called_once_with(
+        source_bucket_key=source_filename,
+        dest_bucket_key=dest_filename,
+        source_bucket_name=MINIO_BUCKET,
+        dest_bucket_name=MINIO_BUCKET,
+    )
+    mock_s3hook.delete_objects.assert_called_once_with(
+        bucket=MINIO_BUCKET,
+        keys=source_filename,
     )
