7a19da4251cb396263a2011e812a52df554137ae
joao-amoedo <jp.advc@gmail.com>
2024-05-12 19:54:06 -0300

    fix(dag_matomo_segmnetation.py): atualizar para coletar os dados em vez de criar segmentacao

--- dags/data_warehouse/dag_matomo_segmentation.py
+++ dags/data_warehouse/dag_matomo_segmentation.py
@@ -1,17 +1,16 @@
 import logging
 from datetime import datetime, timedelta
 from io import StringIO
-from pathlib import Path
+from typing import List, Tuple
 
 import pandas as pd
 import requests
 from airflow.decorators import dag, task, task_group
 from airflow.hooks.base_hook import BaseHook
-from airflow.models import Variable
 from airflow.operators.empty import EmptyOperator
-
-from plugins.graphql.hooks.graphql_hook import GraphQLHook
-
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+from data_warehouse.tools import add_temporal_columns
+from airflow.hooks.postgres_hook import PostgresHook
 default_args = {
     "owner": "airflow",
     "email_on_failure": False,
@@ -20,21 +19,7 @@ default_args = {
     "retry_delay": timedelta(minutes=5),
 }
 DECIDIM_CONN_ID = "api_decidim"
-
-
-def _get_query(space):
-    """
-    Retrieves the query from the specified file and returns it.
-
-    Returns:
-    -------
-      str: The query string.
-    """
-    query = Path(__file__).parent.joinpath(f"./queries/{space}_slug_id.gql").open().read()
-    return query
-
-
-ESPACOS = ["processes"]
+POSTGRES_CONN_ID = "conn_postgres"
 
 
 def get_credentials_matomo(matomo_conn: str = "matomo_conn"):
@@ -59,6 +44,105 @@ def get_credentials_matomo(matomo_conn: str = "matomo_conn"):
 spaces = {"processes": "participatoryProcesses", "assemblies": "assemblies"}
 
 
+def _task_get_segment_matomo():
+    """
+    Retrieves segment definitions from Matomo API.
+
+    Returns:
+    -------
+        pandas.Series: A series containing the segment definitions.
+    Raises:
+    ------
+        Exception: If the API request fails.
+    """
+    matomo_url, token_auth, site_id = get_credentials_matomo()
+
+    params = {
+        "module": "API",
+        "method": "SegmentEditor.getAll",
+        "idSite": site_id,
+        "token_auth": token_auth,
+        "format": "csv",
+        "filter_limit": "-1",
+    }
+    try:
+        response = requests.get(matomo_url, params=params)
+        response.raise_for_status()
+        data = StringIO(response.text)
+        df = pd.read_csv(data)
+        segmentations = df["definition"].str.replace("pageUrl=^", "").values.tolist()
+        return segmentations
+    except requests.exceptions.RequestException as e:
+        logging.error("Erro na solicitação HTTP: %s", e)
+        raise e
+
+
+def get_credentials_matomo():
+    matomo_conn = BaseHook.get_connection("matomo_conn")
+    matomo_url = matomo_conn.host
+    token_auth = matomo_conn.password
+    site_id = matomo_conn.login
+    return matomo_url, token_auth, site_id
+
+
+methods = [
+    ("VisitsSummary", "get"),
+    ("Actions", "getPageUrls"),
+    ("Actions", "getPageTitles"),
+    ("Actions", "getOutlinks"),
+    ("Referrers", "getAll"),
+    ("UserCountry", "getCountry"),
+    ("UserCountry", "getRegion"),
+]
+
+
+def _task_get_data_matomo(all_segmentos, method, space, **context):
+    matomo_url, token_auth, site_id = get_credentials_matomo()
+    url_matomo = f"https://brasilparticipativo.presidencia.gov.br/{space}/"
+    segmentos = [segmento for segmento in all_segmentos if segmento.startswith(url_matomo)]
+    
+    results = {}
+    date = context["execution_date"].strftime("%Y-%m-%d")
+
+    for segmento in segmentos:
+        params = {
+            "module": "API",
+            "idSite": site_id,
+            "period": "day",
+            "date": date,
+            "format": "csv",
+            "token_auth": token_auth,
+            "segment": f"pageUrl=={segmento}",
+            "method": f"{method[0]}.{method[1]}",
+        }
+        response = requests.get(matomo_url, params=params)
+
+        dfs = []
+        try:
+            response.raise_for_status()
+            results[segmento] = response.text
+            data = StringIO(response.text)
+            df = pd.read_csv(data)
+            df["space"] = space
+            df["segment"] = segmento
+            df["method"] = method[1]
+            df["date"] = date
+            dfs.append(df)
+
+        except requests.exceptions.RequestException:
+            logging.error("Erro na solicitação, segmento: %s", segmento)
+    return pd.concat(dfs)
+
+
+MINIO_CONN = "minio_conn_id"
+components = ["assemblies", "processes"]
+BUCKET_NAME = "teste-bucket"
+
+def get_file_name(space: str, method: Tuple[str, str], context: dict) -> str:
+    name_file = f"{space}/{context['ds_nodash']}/{method[1]}_{method[0]}.json"
+    return name_file
+
+
 @dag(
     default_args=default_args,
     schedule_interval="@daily",
@@ -66,182 +150,62 @@ spaces = {"processes": "participatoryProcesses", "assemblies": "assemblies"}
     catchup=False,
     tags=["matomo", "segmentation"],
 )
-def dag_matomo_segmentation():
+def components_matomo():
     start = EmptyOperator(task_id="start")
     end = EmptyOperator(task_id="end")
-    for space in spaces:
+
+    @task
+    def get_segment_matomo():
+        return _task_get_segment_matomo()
+
+    segmento = get_segment_matomo()
+    start >> segmento
+    get_dados_list = []
+
+    for space in components:
 
         @task_group(group_id=space)
         def group(space):
-            start = EmptyOperator(task_id="start")
-            end = EmptyOperator(task_id="end")
+            for method in methods:
 
-            @task(provide_context=True)
-            def get_url_matomo(space, **context):
-                """
-                Retrieves the URLs for Matomo segmentation based on the provided space.
+                @task(task_id=f"save_minio_{method[1]}_{method[0]}", pool="matomo_pool", provide_context=True)
+                def get_data_matomo(segmentos: List[str], space: str, method: tuple, **context):
+                    data = _task_get_data_matomo(segmentos, method, space, **context)
+                    data = add_temporal_columns(data, context['execution_date'])
 
-                Args:
-                ----
-                    space (str): The space for which the URLs need to be retrieved.
-                    context (dict): The context dictionary containing task information.
-
-                Returns:
-                -------
-                    list: A list of URLs for Matomo segmentation.
-
-                """
-                slug_id = context["ti"].xcom_pull(task_ids=f"{space}.get_slug_id")
-                urls = []
-                bp = Variable.get("url_bp")
-                for item in slug_id:
-                    slug = item[0]
-                    component_id = item[1]
-                    url = f"{bp}/{space}/{slug}/f/{component_id}/"
-                    urls.append(url)
-                return urls
-
-            @task
-            def get_segment_matomo():
-                """
-                Retrieves segment definitions from Matomo API.
-
-                Returns:
-                -------
-                    pandas.Series: A series containing the segment definitions.
-
-                Raises:
-                ------
-                    Exception: If the API request fails.
-                """
-                matomo_url, token_auth, site_id = get_credentials_matomo()
-
-                params = {
-                    "module": "API",
-                    "method": "SegmentEditor.getAll",
-                    "idSite": site_id,
-                    "token_auth": token_auth,
-                    "format": "csv",
-                    "filter_limit": "-1",
-                }
-                response = requests.get(matomo_url, params=params)
-                if response.status_code == 200:
-                    data = StringIO(response.text)
-                    df = pd.read_csv(data)
-                    segmentations = df["definition"].str.replace("pageUrl=^", "").values.tolist()
-                    return segmentations
-                else:
-                    raise Exception("deu ruim", response.status_code)
-
-            @task
-            def get_slug_id(space):
-                """
-                Retrieves the slug and ID for a given space using GraphQL.
-
-                Args:
-                ----
-                    space (str): The name of the space.
-
-                Returns:
-                -------
-                    list: A list of tuples containing the slug and ID for each component in the space.
-                """
-                hook = GraphQLHook(DECIDIM_CONN_ID)
-                session = hook.get_session()
-                query = _get_query(space)
-                response = session.post(
-                    hook.api_url,
-                    json={
-                        "query": query,
-                    },
-                )
-                data = eval(response.text)
-                data = data["data"][spaces[space]]
-                slug_id = []
-                for item in data:
-                    slug = item["slug"]
-                    components = item["components"]
-                    for component in components:
-                        _id = component["id"]
-                        slug_id.append((slug, _id))
-                return slug_id
-
-            @task(provide_context=True)
-            def filter_url(space, **context):
-                """
-                Filter URLs based on segments.
-
-                Args:
-                ----
-                    space (str): The space identifier.
-                    **context: Additional context provided by Airflow.
-
-                Returns:
-                -------
-                    list: A list of URLs that are not present in the segments.
-
-                """
-                urls = context["ti"].xcom_pull(task_ids=f"{space}.get_url_matomo")
-                segments = context["ti"].xcom_pull(task_ids=f"{space}.get_segment_matomo")
-
-                new_segments = set(urls).difference(set(segments))
-                return list(new_segments)
-
-            @task(provide_context=True)
-            def add_segmentation(space, **context):
-                """
-                Add segmentation to Matomo.
-
-                Args:
-                ----
-                    space (str): The space name.
-                    **context: Additional context provided by Airflow.
-
-                Returns:
-                -------
-                    str: The response text if the request is successful.
-
-                Raises:
-                ------
-                    Exception: If the request fails with a non-200 status code.
-                """
-                segmentations = context["ti"].xcom_pull(task_ids=f"{space}.filter_url")
-                matomo_url, token_auth, site_id = get_credentials_matomo()
-                for segmentation in segmentations:
-                    splited_segmentation = segmentation.split("/")
-                    name = (
-                        f"component_{splited_segmentation[-5]}_"
-                        f"{splited_segmentation[-4]}_{splited_segmentation[-2]}"
+                    filename = get_file_name(space, method, context)
+                    S3Hook(aws_conn_id=MINIO_CONN).load_string(
+                        string_data=data.to_json(orient="records"),
+                        key=filename,
+                        bucket_name=BUCKET_NAME,
+                        replace=True,
                     )
-                    name = name.replace("-", "_")
-                    # hardcode triste.. mas necessario:(
-                    if not segmentation.startswith("https://brasilparticipativo.presidencia.gov.br/"):
-                        logging.warning("Segmentation not accepted: %s \t - %s", segmentation, name)
-                        continue
-                    params = {
-                        "module": "API",
-                        "method": "SegmentEditor.add",
-                        "idSite": site_id,
-                        "token_auth": token_auth,
-                        "autoArchive": 1,
-                        "format": "csv",
-                        "name": name,
-                        "definition": f"pageUrl=^{segmentation}",
-                    }
-                    response = requests.post(matomo_url, params=params)
-                    if response.status_code == 200:
-                        logging.info("Segmentation added: %s", segmentation)
-                    else:
-                        raise Exception("deu ruim", response.status_code)
 
-            _get_slug_id = get_slug_id(space)
-            _get_url_matomo = get_url_matomo(space)
-            _get_segment_matomo = get_segment_matomo()
-            _filter_url = filter_url(space)
-            (start >> [_get_url_matomo, _get_segment_matomo] >> _filter_url >> add_segmentation(space) >> end)
-            start >> _get_slug_id >> _get_url_matomo
+# 
+                @task(task_id=f"save_postgres_{method[1]}_{method[0]}", provide_context=True)
+                def _task_save_postgres(space, method, **context):
+                    filename = get_file_name(space, method, context)
+                    
+                    key = S3Hook(aws_conn_id=MINIO_CONN).read_key(
+                        key=filename,
+                        bucket_name=BUCKET_NAME,
+                    )
+                    df = pd.read_json(key)
+                    engine = PostgresHook(postgres_conn_id=POSTGRES_CONN_ID).get_sqlalchemy_engine()
+                    df.to_sql(
+                        name=f"teste",
+                        con=engine,
+                        if_exists="append",
+                        index=False,
+                        schema='raw',
+                    )
+                    return df
 
-        start >> group(space) >> end
+                get_data_matomo(segmento, space, method) >> _task_save_postgres(space, method)
 
+        group(space=space) >> end
+        # group(space) >> end
 
-dag = dag_matomo_segmentation()
+    
+
+components_matomo()
