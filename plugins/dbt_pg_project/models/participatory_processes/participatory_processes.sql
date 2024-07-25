20f43fc8f7a59402a3d196f0d044c48bb629108c
Eric Silveira <silveirames@hotmail.com>
2024-07-25 12:59:05 -0300

    feat(dbt_participatory_processes): adicionando total de propostas por processo participativo

--- plugins/dbt_pg_project/models/participatory_processes/participatory_processes.sql
+++ plugins/dbt_pg_project/models/participatory_processes/participatory_processes.sql
@@ -15,12 +15,24 @@ WITH recent_processes AS (
        ROW_NUMBER() OVER (PARTITION BY main_title ORDER BY proposal_updated_at DESC) AS rn
    FROM
        {{ source('raw', 'updated_proposals') }}
+),
+total_proposals_per_processes AS (
+   SELECT
+       main_title,
+       COUNT(proposal_id) AS total_proposals
+   FROM
+       {{ source('raw', 'updated_proposals') }}
+   GROUP BY
+       main_title
 )
 SELECT
    rp.main_title,
-   rp.latest_updates
+   rp.latest_updates,
+   tppp.total_proposals
 FROM
    recent_processes rp
+JOIN
+   total_proposals_per_processes tppp ON rp.main_title = tppp.main_title
 
 
 
