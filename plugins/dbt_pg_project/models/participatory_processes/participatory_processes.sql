9cda44e666b7ae2de32025931f726f5954950f81
Eric Silveira <silveirames@hotmail.com>
2024-07-25 12:58:12 -0300

    feat(dbt_participatory_processes): adicionando titulos dos processos participativos e ultimas atualizacoes

--- plugins/dbt_pg_project/models/participatory_processes/participatory_processes.sql
+++ plugins/dbt_pg_project/models/participatory_processes/participatory_processes.sql
@@ -8,6 +8,20 @@
 }}
 
 
+WITH recent_processes AS (
+   SELECT
+       main_title,
+       proposal_updated_at AS latest_updates,
+       ROW_NUMBER() OVER (PARTITION BY main_title ORDER BY proposal_updated_at DESC) AS rn
+   FROM
+       {{ source('raw', 'updated_proposals') }}
+)
+SELECT
+   rp.main_title,
+   rp.latest_updates
+FROM
+   recent_processes rp
+
 
 
 
