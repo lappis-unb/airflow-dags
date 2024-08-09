a1f177df8423aee0af61a9569e56af21d6198c7f
Wolffstein <wolffriederichstein@gmail.com>
2024-08-09 00:14:12 -0400

    feat(dbt-processo-matomo): reorganizando a query de devices_detection matomo

--- plugins/dbt_pg_project/models/matomo/devices_detection_get_type/devices_detection_get_type_month.sql
+++ plugins/dbt_pg_project/models/matomo/devices_detection_get_type/devices_detection_get_type_month.sql
@@ -8,31 +8,23 @@
 
 
 SELECT 
-	SUBSTRING(url FROM LENGTH('pageUrl==') + 1) AS DSC_URL,
-    SPLIT_PART(SUBSTRING(url FROM LENGTH('pageUrl==https://brasilparticipativo.presidencia.gov.br/') + 1), '/', 2) AS DSC_texto_participativo,
-    SPLIT_PART(SUBSTRING(url FROM LENGTH('pageUrl==https://brasilparticipativo.presidencia.gov.br/') + 1), '/', 4) AS DSC_processo_id,
-    "label" AS DSC_DISPOSITIVO,
-    nb_uniq_visitors AS NUM_VISITANTES_UNICOS,
-    nb_visits AS NUM_TOTAL_VISITAS,
-    nb_actions AS NUM_TOTAL_ACOES,
+	
+	space AS TIPO_ESPACO,
     nb_users AS NUM_TOTAL_USUARIOS,
-    max_actions AS NUM_MAX_ACOES_POR_VISITA,
-    sum_visit_length AS NUM_DURACAO_TOTAL_VISITAS,
-    bounce_count AS NUM_TOTAL_REJEICOES,
+    nb_visits AS NUM_TOTAL_VISITAS,
+    nb_uniq_visitors AS NUM_VISITANTES_UNICOS,
     nb_visits_converted AS NUM_VISITAS_CONVERTIDAS,
-    segment AS DSC_SEGMENTO,
-    logo AS DSC_LOGO,
-    "space" AS DSC_ESPACO_PARTICIPATIVO,
-    "method" AS DSC_METODO,
-    "date" AS DAT_DATA,
-    event_day_id AS DAT_ID_DIA_EVENTO,
-    available_day_id AS DAT_ID_DIA_DISPONIVEL,
-    available_month_id AS DAT_ID_MES_DISPONIVEL,
-    available_year_id AS DAT_ID_ANO_DISPONIVEL,
-    writing_day_id AS DAT_ID_DIA_ESCRITA,
-    "period" AS DSC_PERIODO,
-    sum_daily_nb_users AS NUM_TOTAL_USUARIOS_DIARIOS,
-    sum_daily_nb_uniq_visitors AS NUM_VISITANTES_UNICOS_DIARIOS
+    sum_visit_length AS NUM_DURACAO_TOTAL_VISITAS,
+    max_actions AS NUM_MAX_ACOES_POR_VISITA,
+    avg_time_on_site AS NUM_TEMPO_MEDIO_NO_SITE,
+    bounce_count AS NUM_TOTAL_REJEICOES,
+    bounce_rate AS NUM_TAXA_REJEICAO,
+    nb_actions AS NUM_TOTAL_ACOES,
+    nb_actions_per_visit AS NUM_ACOES_POR_VISITA,
+    date AS DAT_DATA_VISITA,
+    SUBSTRING(url FROM LENGTH('pageUrl==') + 1) AS DSC_URL_PAGINA,
+    SPLIT_PART(SUBSTRING(url FROM LENGTH('pageUrl==https://brasilparticipativo.presidencia.gov.br/') + 1), '/', 2) AS DSC_NOME_SECAO,
+    SPLIT_PART(SUBSTRING(url FROM LENGTH('pageUrl==https://brasilparticipativo.presidencia.gov.br/') + 1), '/', 4) AS DSC_ID_PROCESSO
 FROM 
     {{ source('raw', 'devices_detection_get_type') }}
 where period = 'month'
