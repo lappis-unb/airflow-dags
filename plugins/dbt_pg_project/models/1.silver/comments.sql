a1b371ae37611b6a2470cb1d1feae3b9a659203b
Matheus Beltrami <matheus.beltrami@dexlabs.io>
2024-08-12 08:56:38 -0300

    release(silver model) Cria uma branch de release para o modelo Silver do decidim

---
+++ plugins/dbt_pg_project/models/1.silver/comments.sql
@@ -0,0 +1,28 @@
+WITH
+
+comments_base as
+(
+    SELECT
+        *,
+        row_number() over(partition by id order by updated_at DESC) as row_number
+    FROM
+        {{ source('bronze', 'decidim_comments_comments') }}
+    WHERE
+        decidim_participatory_space_type = 'Decidim::ParticipatoryProcess'
+)
+
+select
+	id as comment_id,
+	decidim_author_id as user_id,
+	lower((regexp_split_to_array(decidim_commentable_type, '::'))[array_length(regexp_split_to_array(decidim_commentable_type, '::'), 1)]) as component_type,
+	decidim_commentable_id as commented_component_id,
+	lower((regexp_split_to_array(decidim_root_commentable_type, '::'))[array_length(regexp_split_to_array(decidim_root_commentable_type, '::'), 1)]) as root_component_type,
+	decidim_root_commentable_id as commented_root_component_id,
+	body::json->>'pt-BR' as comment_text,
+	created_at,
+	updated_at,
+	deleted_at
+from
+	comments_base
+WHERE
+    row_number = 1
---
+++ plugins/dbt_pg_project/models/1.silver/proposals.sql
@@ -0,0 +1,73 @@
+WITH
+
+proposals_base as
+(
+    SELECT
+        *,
+        row_number() over(partition by id order by updated_at DESC) as row_number
+    FROM
+        {{ source('bronze', 'decidim_proposals_proposals') }}
+),
+
+deduped_proposals as (select * from proposals_base where row_number = 1),
+
+scopes_base as
+(
+    SELECT
+        *,
+        row_number() over(partition by id, decidim_organization_id order by updated_at DESC) as row_number
+    FROM
+        {{ source('bronze', 'decidim_scopes') }}
+    WHERE
+        -- Filter only Brasil Participativo
+        decidim_organization_id = 1
+),
+
+deduped_scopes as (select * from scopes_base where row_number = 1),
+
+components_base as
+(
+    SELECT
+        *,
+        row_number() over(partition by id order by updated_at DESC) as row_number
+    FROM
+        {{ source('bronze', 'decidim_components') }}
+),
+
+deduped_components as (select * from components_base where row_number = 1),
+
+coauthorships_base as
+(
+    SELECT
+        *,
+        row_number() over(partition by id order by updated_at DESC) as row_number
+    FROM
+        {{ source('bronze', 'decidim_coauthorships') }}
+    WHERE
+        coauthorable_type = 'Decidim::Proposals::Proposal'
+),
+
+deduped_coauthorships as (select * from coauthorships_base where row_number = 1)
+
+select
+	p.id as proposal_id,
+    c.participatory_space_id as process_id,
+    c.participatory_space_type,
+    ca.decidim_author_id as user_id,
+    ca.decidim_author_type as author_type,
+	p.state as proposal_status,
+	p.created_at,
+	p.title::json->>'pt-BR' as proposal_title,
+	p.body::json->>'pt-BR' as proposal_text,
+	s.name::json->>'pt-BR' as proposal_scope
+from 
+	deduped_proposals p
+		left join
+	deduped_scopes s
+			on p.decidim_scope_id = s.id
+        left join
+    deduped_components c
+            on p.decidim_component_id = c.id
+        left join
+    deduped_coauthorships ca
+            on p.id = ca.coauthorable_id
