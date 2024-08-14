bfefdfbad32c2d88d1dace79b0e6f6512e1f79a7
Wolffstein <wolffriederichstein@gmail.com>
2024-08-14 14:14:44 -0400

    feat(dbt-processo-matomo): Refatorando user_country_get_region

--- plugins/dbt_pg_project/models/matomo/user_country_get_region/user_country_get_region_day.sql
+++ plugins/dbt_pg_project/models/matomo/user_country_get_region/user_country_get_region_day.sql
@@ -8,34 +8,20 @@
 
 
   SELECT 
-    SUBSTRING(url FROM LENGTH('pageUrl==') + 1) AS DSC_URL,
-    SPLIT_PART(SUBSTRING(url FROM LENGTH('pageUrl==https://brasilparticipativo.presidencia.gov.br/') + 1), '/', 2) AS DSC_TEXTO_PARTICIPATIVO,
-    SPLIT_PART(SUBSTRING(url FROM LENGTH('pageUrl==https://brasilparticipativo.presidencia.gov.br/') + 1), '/', 4) AS DSC_PROCESSO_ID,
-    "label" AS DSC_ETIQUETA,
+        SPLIT_PART(SUBSTRING(url FROM LENGTH('pageUrl==https://brasilparticipativo.presidencia.gov.br/') + 1), '/', 4) AS PROCESSO_ID,
+    country_name AS DSC_NOME_PAIS,
+    region_name AS DSC_NOME_REGIAO,
+    region AS DSC_ESTADO,
     nb_uniq_visitors AS NUM_VISITANTES_UNICOS,
     nb_visits AS NUM_TOTAL_VISITAS,
-    nb_actions AS NUM_TOTAL_ACOES,
     nb_users AS NUM_TOTAL_USUARIOS,
     max_actions AS NUM_MAX_ACOES_POR_VISITA,
     sum_visit_length AS NUM_DURACAO_TOTAL_VISITAS,
-    bounce_count AS NUM_TOTAL_REJEICOES,
+    nb_actions AS NUM_TOTAL_ACOES,
     nb_visits_converted AS NUM_VISITAS_CONVERTIDAS,
-    region AS DSC_REGIAO,
-    country AS DSC_PAIS,
-    country_name AS DSC_NOME_PAIS,
-    region_name AS DSC_NOME_REGIAO,
-    logo AS DSC_LOGO,
-    "space" AS DSC_ESPACO_PARTICIPATIVO,
-    "method" AS DSC_METODO,
+    bounce_count AS NUM_TOTAL_REJEICOES,
     "date" AS DAT_REF,
-    event_day_id AS DAT_ID_DIA_EVENTO,
-    available_day_id AS DAT_ID_DIA_DISPONIVEL,
-    available_month_id AS DAT_ID_MES_DISPONIVEL,
-    available_year_id AS DAT_ID_ANO_DISPONIVEL,
-    writing_day_id AS DAT_ID_DIA_ESCRITA,
-    "period" AS DSC_PERIODO,
-    sum_daily_nb_uniq_visitors AS NUM_VISITANTES_UNICOS_DIARIOS,
-    sum_daily_nb_users AS NUM_TOTAL_USUARIOS_DIARIOS
+    SUBSTRING(url FROM LENGTH('pageUrl==') + 1) AS DSC_URL
 FROM 
     {{ source('raw', 'user_country_get_region') }}
 where period = 'day'
