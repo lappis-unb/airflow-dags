4ecee9ed8b9967f82a741c1a4570fb19f502f8ae
joao_amoedo <jp.advc@gmail.com>
2024-09-16 10:03:22 -0300

    refactor(datas.sql) adiciona distinct e testes

--- plugins/dbt_pg_project/models/1.silver/datas.sql
+++ plugins/dbt_pg_project/models/1.silver/datas.sql
@@ -3,21 +3,48 @@
 ) }}
 
 WITH feriados AS (
-    SELECT *,
+    SELECT DISTINCT ON (to_char(date, 'YYYYMMDD')) 
+           name,
            to_char(date, 'YYYYMMDD') AS formatted_date
     FROM {{ source('bronze', 'feriados') }}
+    ORDER BY to_char(date, 'YYYYMMDD'), name
 ),
 datas AS (
-    SELECT *
+    SELECT   ano_mes_dia
+            ,ano_mes
+            ,ano
+            ,descricao_dia_semana
+            ,descricao_mes
+            ,numero_mes
+            ,numero_dia_mes
+            ,numero_dia_semana
+            ,numero_dia_ano
+            ,numero_semana_mes
+            ,final_de_semana
+            ,numero_semana_ano
+
     FROM {{ source('bronze', 'datas') }}
 )
 
-SELECT d.*, 
-       CASE 
-           WHEN f.name IS NOT NULL THEN 1 
-           ELSE 0 
-       END AS flag_feriado, 
-       f.name 
+SELECT 
+
+             d.ano_mes_dia
+            ,d.ano_mes
+            ,d.ano
+            ,d.descricao_dia_semana
+            ,d.descricao_mes
+            ,d.numero_mes
+            ,d.numero_dia_mes
+            ,d.numero_dia_semana
+            ,d.numero_dia_ano
+            ,d.numero_semana_mes
+            ,d.final_de_semana
+            ,d.numero_semana_ano
+            ,CASE 
+                WHEN f.name IS NOT NULL THEN 1 
+                ELSE 0 
+            END AS flag_feriado
+            ,f.name as feriado_nacional
 FROM datas d
 LEFT JOIN feriados f 
 ON d.ano_mes_dia = f.formatted_date
